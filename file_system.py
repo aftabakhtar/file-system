00bde5294848f00b6520ea5415e69b02bcbe9791
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2020-11-24 02:51:41 +0500

    Implement create dir / files and other functions

--- file_system.py
+++ file_system.py
@@ -8,53 +8,108 @@ PAGE_SIZE = 4096
 FILE_SYSTEM = 'sample.fst'
 DIR_DELIMITER = '\$dir$'
 FILE_DELIMITER = '\$file$'
-PROCESS_TABLE = '\$process$'
-MEMORY_TABLE = '\$memory$'
+PROCESS_DELIMITER = '\$process$'
+FRAME_DELIMITER = '\$memory$'
 CWD = '~/'
+DATA = dict()
 
 
 def read_system():
     try:
         with open(FILE_SYSTEM, 'r') as fst:
             file_system_data = fst.read()
-            list_dir(file_system_data)
+            get_data(file_system_data)
 
     except IOError:
         fst = open(FILE_SYSTEM, 'w')
-        fst.write(DIR_DELIMITER + CWD + DIR_DELIMITER)
+        get_data(DIR_DELIMITER + CWD + '\,' + DIR_DELIMITER)
+        update_system()
         fst.close()
         print('File created successfully!')
 
 
 def update_system():
-    pass
+    global DATA
+    directories = DATA['dir']
+    files = DATA['files']
+    process = DATA['process']
+    frames = DATA['frames']
 
-
-def list_dir(file_system_data):
-    try:
-        directories = file_system_data.split(DIR_DELIMITER)[1].split(',')
-
-    except IndexError:
-        directories = ''
+    # print(directories, files, process, frames)
 
     try:
-        files = file_system_data.split(FILE_DELIMITER)[1].split(',')
+        with open(FILE_SYSTEM, 'w') as fst:
+            fst.write(DIR_DELIMITER)
+            for directory in directories:
+                fst.write("%s\," % directory)
+            fst.write(DIR_DELIMITER + '\n')
 
+            fst.write(FILE_DELIMITER)
+            for file in files:
+                fst.write("%s\," % file)
+            fst.write(FILE_DELIMITER + '\n')
+
+            fst.write(PROCESS_DELIMITER)
+            for p in process:
+                fst.write('%s\,' % p)
+            fst.write(PROCESS_DELIMITER + '\n')
+
+            fst.write(FRAME_DELIMITER)
+            for f in frames:
+                fst.write("%s\," % f)
+            fst.write(FRAME_DELIMITER)
+    except IOError as e:
+        raise e
+        print('Cannot update system due to some error')
+
+
+def get_data(file_system_data):
+    global DATA
+
+    try:
+        DATA['dir'] = file_system_data.split(DIR_DELIMITER)[1].split('\,')[:-1]
     except IndexError:
-        files = ''
+        DATA['dir'] = []
 
-    print('Directories')
+    try:
+        DATA['files'] = file_system_data.split(FILE_DELIMITER)[1].split('\,')[:-1]
+    except IndexError:
+        DATA['files'] = []
+
+    try:
+        DATA['process'] = file_system_data.split(PROCESS_DELIMITER)[1].split('\,')[:-1]
+    except IndexError:
+        DATA['process'] = []
+
+    try:
+        DATA['frames'] = file_system_data.split(FRAME_DELIMITER)[1].split('\,')[:-1]
+    except IndexError:
+        DATA['frames'] = []
+
+
+def list_dir():
+    global DATA
+    directories = DATA['dir']
+    files = DATA['files']
+
     for x in directories:
-        print(x)
-
-    print()
+        # x = x.split(CWD)[1]
+        print(u"\U0001F4C2", x)
 
     for x in files:
-        print(x)
+        # x = x.split(CWD)[1]
+        print(u"\U0001F5C4", x)
 
 
 def create(file_name):
-    pass
+    global DATA
+    new_file = CWD + file_name
+
+    if new_file not in DATA['files']:
+        DATA['files'] += [new_file]
+        update_system()
+    else:
+        print('create(): cannot make file due to duplicate')
 
 
 def delete(file_name):
@@ -62,15 +117,47 @@ def delete(file_name):
 
 
 def mk_dir(dir_name):
-    pass
+    global DATA
+    new_directory = CWD + dir_name
+
+    if new_directory not in DATA['dir'] and new_directory not in DATA['files']:
+        DATA['dir'] += [new_directory]
+        update_system()
+    else:
+        print('mk_dir(): cannot make directory due to duplicate')
 
 
 def ch_dir(dir_name):
-    pass
+    global CWD
+
+    if CWD + dir_name in DATA['dir']:
+        CWD = CWD + dir_name + '/'
+        print('successfully changed directory to %s' % dir_name)
+        print('cwd: %s' % CWD)
+    else:
+        print('ch_dir(): cannot change directory, it does not exist')
 
 
 def move(source, destination):
-    pass
+    if source in DATA['files'] and destination in DATA['dir']:
+        file_name = source.split('/')[-1]
+
+        if destination.endswith('/'):
+            file_name = destination + file_name
+        else:
+            file_name = destination + '/' + file_name
+
+        if file_name not in DATA['dir'] and file_name not in DATA['files']:
+            DATA['files'] += [file_name]
+            DATA['files'].remove(source)
+            update_system()
+            print("successfully moved the data from {} to {}".format(source, destination))
+        else:
+            print("move(): make sure file names are not duplicating")
+
+    else:
+        print('move(): error, make sure that source and destination are correct')
+
 
 
 def open_file(file_name):
--- run.py
+++ run.py
@@ -4,3 +4,8 @@ This script will be used to test the file system created
 from file_system import *
 
 read_system()
+# ch_dir('example12')
+# create('example')
+# mk_dir('demo')
+move('~/example', '~/demo')
+list_dir()
